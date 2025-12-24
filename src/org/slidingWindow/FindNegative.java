ca2f1cf3e60ddd00e749b1486f8eb3130b7a2a70
khotAkash07 <khotakash5737@gmail.com>
2025-12-24 10:48:58 +0530

    Array Sliding window problem solve

--- src/org/slidingWindow/FindNegative.java
+++ src/org/slidingWindow/FindNegative.java
@@ -1,10 +1,13 @@
+package org.slidingWindow;
+
 import java.util.*;
 
-public class FindNegative{
-	public static void main(String args[]){
-		
-		int	k = 3;
-		boolean flag;
+public class FindNegative {
+
+	public static void main(String[] args) {
+
+		int k = 3;
+
 		ArrayList<Integer> al = new ArrayList<>();
 		al.add(12);
 		al.add(-1);
@@ -14,92 +17,48 @@ public class FindNegative{
 		al.add(30);
 		al.add(16);
 		al.add(28);
-		
-		List<Integer> result = getNegative(al,k);
-		System.out.println(result);
-		
 
-		/******************************************************
-		ArrayDeque<Integer> ad = new ArrayDeque<>();
-		for(int i=0; i<=al.size()-k; i++){
-			flag = false;
-			int count = i;
-			while(count < k+i){
-				//System.out.println(al.get(count));
-				if(al.get(count) < 0){
-					ad.push(al.get(count));
-					flag = true;
-					break;
-				}
-				count++;
-			}
-			System.out.println(flag);
-			if(!flag) ad.push(0);
-		}
-		System.out.println(ad);
-		***************************************************************/
-	
+		List<Integer> result = getNegative(al, k);
+		System.out.println(result);
 	}
-	public static List<Integer> getNegative(ArrayList<Integer> al, int k){
-		
-		List<Integer> li = new ArrayList<>();
+
+	public static List<Integer> getNegative(ArrayList<Integer> al, int k) {
+
+		List<Integer> result = new ArrayList<>();
 		Deque<Integer> dq = new LinkedList<>();
-		
-		int i=0, j=0;
-		
-		while( j < al.size() ){
-			if(al.get(j) < 0){
+
+		int i = 0, j = 0;
+
+		while (j < al.size()) {
+
+			// Step 1: add negative index
+			if (al.get(j) < 0) {
 				dq.addLast(j);
 			}
-			
-			if(j - i + 1 < k){j++;}
-			else if(j - i + 1 == k){
-				
-				if(!dq.isEmpty() && dq.peekFirst() >= i){
-					li.add(al.get(dq.peekFirst()));
-				}else{
-					li.add(0);
-				}
+
+			// Step 2: window size not reached
+			if (j - i + 1 < k) {
+				j++;
 			}
-			
-			if(!dq.isEmpty() && dq.peekFirst() == i){
-				dq.pollFirst();
-			}	
-			i++;
-			j++;
+
+			// Step 3: window size reached
+			else if (j - i + 1 == k) {
+
+				if (!dq.isEmpty()) {
+					result.add(al.get(dq.peekFirst()));
+				} else {
+					result.add(0);
+				}
+
+				// Step 4: remove outgoing element
+				if (!dq.isEmpty() && dq.peekFirst() == i) {
+					dq.pollFirst();
+				}
+
+				i++;
+				j++;
+			}
 		}
-		
-		List<Integer> ans = new ArrayList<>();
-        Deque<Integer> dq = new LinkedList<>();
-
-        int i = 0, j = 0;
-
-        while (j < arr.length) {
-            // Add index of negative number to deque
-            if (arr[j] < 0) {
-                dq.addLast(j);
-            }
-
-            // Check if window has reached size k
-            if (j - i + 1 < k) {
-                j++;
-            } else if (j - i + 1 == k) {
-                // Window is of size k
-                if (!dq.isEmpty() && dq.peekFirst() >= i) {
-                    ans.add(arr[dq.peekFirst()]);
-                } else {
-                    ans.add(0);
-                }
-
-                // Remove the element going out of the window
-                if (!dq.isEmpty() && dq.peekFirst() == i) {
-                    dq.pollFirst();
-                }
-
-                i++;
-                j++;
-            }
-        }
-		return li;
+		return result;
 	}
 }
--- src/org/slidingWindow/MaxFromSubArray.java
+++ src/org/slidingWindow/MaxFromSubArray.java
@@ -1,6 +1,20 @@
-//Get Max From sub-array
+package org.slidingWindow;
 
-public class MaxFromSubArray{
-	public static void main(String args[]){
-		
-		
+public class MaxFromSubArray {
+	public static void main(String[] args) {
+
+		int[] arr = {1, 3, 2, 5, 4};
+		int k = 3;
+
+		for (int i = 0; i <= arr.length - k; i++) {
+			int max = arr[i];
+
+			for (int j = i; j < i + k; j++) {
+				if (arr[j] > max) {
+					max = arr[j];
+				}
+			}
+			System.out.print(max + " ");
+		}
+	}
+}

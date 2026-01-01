3bfa2b042c993abc5dc139841c50487e7ccdc49f
khotAkash07 <khotakash5737@gmail.com>
2026-01-01 20:07:16 +0530

    Practice on Recursion

---
+++ src/com/DSA/Recursion.java
@@ -0,0 +1,17 @@
+package com.DSA;
+
+public class Recursion {
+    static int sum;
+    public static void main(String[] args) {
+        findSum(5);
+    }
+
+    public static void findSum(int n){
+        if(n==0){
+            System.out.println(sum);
+            return;
+        }
+        sum +=n;
+        findSum(n-1);
+    }
+}

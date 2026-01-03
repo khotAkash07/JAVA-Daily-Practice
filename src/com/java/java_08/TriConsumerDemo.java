8720343cbce0ba184e45319d8eaa42a35520e33b
khotAkash07 <khotakash5737@gmail.com>
2026-01-03 23:18:26 +0530

    TriConsumer Self Creater

--- src/com/java/java_08/TriConsumerDemo.java
+++ src/com/java/java_08/TriConsumerDemo.java
@@ -1,7 +1,11 @@
 package com.java.java_08;
 
-class TriConsumer{
-
+@FunctionalInterface
+interface TriConsumer<A,B,R>{
+    R accept(A a,B b);
 }
 public class TriConsumerDemo {
+    public static void main(String[] args) {
+        System.out.println(((TriConsumer<Integer,Integer,Integer>)(Integer::sum)).accept(10,20));
+    }
 }

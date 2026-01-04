e08dbea2c320aa63ad5e43b265dd06f2d538e110
khotAkash07 <khotakash5737@gmail.com>
2026-01-04 23:14:13 +0530

    Find Occurance Using Stream

--- src/com/java/collection/FindFrequency.java
+++ src/com/java/collection/FindFrequency.java
@@ -1,4 +1,17 @@
 package com.java.collection;
 
-public class fd {
+import java.util.*;
+
+public class FindFrequency {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("A", "B", "A", "C", "B");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s : list) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
 }
--- src/com/java/java_08/FindDuplicate.java
+++ src/com/java/java_08/FindDuplicate.java
@@ -1,4 +1,16 @@
 package com.java.java_08;
 
-public class df {
+import java.util.*;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,2,4,1);
+        Set<Integer> set = new HashSet<>();
+
+        list.stream()
+                .filter(n -> !set.add(n))
+                .forEach(System.out::println);
+    }
 }
+
+

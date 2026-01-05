5bdd8944b0a39fde0b509b238a5b8fa24edfbdea
khotAkash07 <khotakash5737@gmail.com>
2026-01-05 10:59:51 +0530

    Jdbc Practice

---
+++ src/com/JDBC/MockPractice.java
@@ -0,0 +1,30 @@
+package com.JDBC;
+
+import java.sql.*;
+
+public class MockPractice {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/akash";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, "root", "root");
+            if (conn != null) {
+                PreparedStatement ps = conn.prepareStatement("select * from employee");
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    System.out.print(rs.getInt(1)+" |\t");
+                    System.out.print(rs.getString(2)+"\t|\t");
+                    System.out.print(rs.getInt(3)+"  |");
+                    System.out.println("\n------------------------");
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
+
--- src/com/java/java_08/FindDuplicate.java
+++ src/com/java/java_08/FindDuplicate.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class FindDuplicate {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,2,4,1);
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 4, 1);
         Set<Integer> set = new HashSet<>();
 
         list.stream()
-                .filter(n -> !set.add(n))
+                .filter(x -> !set.add(x))
                 .forEach(System.out::println);
     }
 }

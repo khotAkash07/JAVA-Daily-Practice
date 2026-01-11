0964aef9aaacb94b1b4dd5014e4e7a75e9e2b29e
Akash Khot <khotakash5737@gmail.com>
2026-01-11 23:34:57 +0530

    Update MapDemo.java

--- src/org/Collection/MapDemo.java
+++ src/org/Collection/MapDemo.java
@@ -1,12 +1,9 @@
 package org.Collection;
-
 import java.util.*;
-
 public class MapDemo {
 
     public static Map<Integer, String> mapOperation(){
         Map<Integer,String> map = new HashMap<>();
-       // map.put(1)
         return map;
     }
     public static void main(String[] args) {

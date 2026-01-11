e398a57fc60a9510240e773f4a88652a3a9b9369
Akash Khot <khotakash5737@gmail.com>
2026-01-11 23:37:00 +0530

    Update StreamDemo.java

--- src/org/java_08/StreamDemo.java
+++ src/org/java_08/StreamDemo.java
@@ -6,10 +6,7 @@ import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
-
       //  List.of(10,20,30,40,50,60).stream().map(val -> val*2).forEach(num -> System.out.println(num));
-
-
         List.of(1,2,3,4,5,6,7,8,9).stream().filter(val -> val%2==0).map(m -> m+1).forEach(System.out::println);
     }
 }

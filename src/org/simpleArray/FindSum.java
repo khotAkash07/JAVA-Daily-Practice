c99fa3865fbfc3853fb68baeaa969154780eba06
Akash Khot <khotakash5737@gmail.com>
2026-01-11 23:38:01 +0530

    Update FindSum.java

--- src/org/simpleArray/FindSum.java
+++ src/org/simpleArray/FindSum.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class FindSum {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Any Number : ");
         int n = sc.nextInt();
         int sum = 0;
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 1 ; i <= n ; i++)
             sum += i;
-        }
         System.out.println(sum);
     }
 }

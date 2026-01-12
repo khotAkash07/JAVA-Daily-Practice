96ca920c73298b625d92a4c5afceec14cbf51fcc
khotAkash07 <khotakash5737@gmail.com>
2026-01-12 22:30:49 +0530

    Predicate Interface

--- src/com/java/java_08/LambdaExpressionApp.java
+++ src/com/java/java_08/LambdaExpressionApp.java
@@ -1,5 +1,7 @@
 package com.java.java_08;
 
+import java.util.function.Predicate;
+
 @FunctionalInterface
 interface MathOperations {
     int operation(int a, int b);
@@ -7,7 +9,12 @@ interface MathOperations {
 public class LambdaExpressionApp {
     public static void main(String[] args) {
 
-       // MathOperations sumOps = (a, b) -> a+b;
+        // MathOperations sumOps = (a, b) -> a+b;
         System.out.println(((MathOperations)((a, b) -> a+b)).operation(15, 20));
+
+        Predicate<Integer> isEven = (a) -> a % 2 == 0;
+        boolean b = isEven.test(13);
+        System.out.println(b?"Even":"Odd");
+
     }
 }
